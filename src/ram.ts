538f328e072b55d373167c0f826e6268f9943c4b
Ivan Chub <ivansergchub@gmail.com>
2016-02-22 20:32:11 -0800

    adding ram

--- src/ram.ts
+++ src/ram.ts
@@ -1,3 +1,11 @@
 export class RAM {
+    private _memory:number[] = Array.from(new Array(RAM.size), () => 0);
 
+    public static get size() {
+        return Math.pow(2, 10);
+    }
+
+    get memory():number[] {
+        return this._memory;
+    }
 }
