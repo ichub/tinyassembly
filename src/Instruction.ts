c36f3d1d6e970196c1b7d7b48533afd7f20ecd88
Ivan Chub <ivansergchub@gmail.com>
2016-02-27 00:09:15 -0800

    added parameter verification

--- src/Instruction.ts
+++ src/Instruction.ts
@@ -1,5 +1,7 @@
 import {IInstructionOperation} from "./IInstructionOperation";
 import {ParamType} from "./ParamType";
+import {CPU} from "./CPU";
+import {Register} from "./Register";
 
 export class Instruction {
     private _name:string;
@@ -14,12 +16,41 @@ export class Instruction {
         this._paramList = Object.freeze(paramList);
     }
 
+    private verifySingleParam(type:ParamType, value:number):boolean {
+        switch (type) {
+            case ParamType.Value:
+                return true;
+            case ParamType.Register:
+                return value <= Register.maxValue;
+            case ParamType.None:
+                return value === 0;
+            default:
+                return false;
+        }
+    }
+
+    private verifyParams(cpu:CPU):boolean {
+        for (let i = 0; i < this._paramList.length; i++) {
+            if (!this.verifySingleParam(this._paramList[i], cpu.params.raw[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public get opcode():number {
         return this._opcode;
     }
 
     public get operation():IInstructionOperation {
-        return this._operation;
+        return (cpu:CPU) => {
+            if (!this.verifyParams(cpu)) {
+                throw "params were not correct";
+            }
+
+            this._operation(cpu);
+        };
     }
 
     get name():string {
