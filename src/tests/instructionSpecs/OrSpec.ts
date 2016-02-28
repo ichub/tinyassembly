4bac50429c6fe160e39ae7efd2136587b4515444
Ivan Chub <ivansergchub@gmail.com>
2016-02-27 17:39:30 -0800

    or spec

---
+++ src/tests/instructionSpecs/OrSpec.ts
@@ -0,0 +1,28 @@
+import {Computer} from "../../computer";
+
+describe("the OR instruction", function () {
+    it("param3 = param1 | param2", function () {
+        const comp = new Computer();
+        const left = 123;
+        const right = 456;
+
+        comp.cpu.registers.A.value = left;
+        comp.cpu.registers.B.value = right;
+        comp.ram.setMemory(0, [7, 0, 1, 2]);
+        comp.cpu.step();
+
+        expect(comp.cpu.registers.C.value).toBe(left | right);
+    });
+
+    it("param3 = param1 | param2", function () {
+        const comp = new Computer();
+        const left = 0b11101;
+        const right = 0b01101;
+
+        comp.cpu.registers.A.value = left;
+        comp.ram.setMemory(0, [8, right, 0, 0]);
+        comp.cpu.step();
+
+        expect(comp.cpu.registers.A.value).toBe(left | right);
+    });
+});
