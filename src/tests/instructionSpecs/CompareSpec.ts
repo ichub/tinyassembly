af01ea1da4b7afe893367d0e8ffc2df797333350
Ivan Chub <ivansergchub@gmail.com>
2016-02-27 17:52:25 -0800

    compare works

--- src/tests/instructionSpecs/CompareSpec.ts
+++ src/tests/instructionSpecs/CompareSpec.ts
@@ -41,17 +41,49 @@ describe("CMP instruction", function () {
         expect(comp.cpu.flags.more).toBe(false);
         expect(comp.cpu.flags.equal).toBe(true);
     });
-    
-    it("register", function () {
-        const comp = new Computer();
-        const left = 123;
-        const right = 456;
 
-        comp.cpu.registers.A.value = left;
-        comp.cpu.registers.B.value = right;
-        comp.ram.setMemory(0, [9, 0, 1, 2]);
+    it("register less", function () {
+        const comp = new Computer();
+        let less = 123;
+        let more = 456;
+
+        comp.cpu.registers.A.value = less;
+        comp.cpu.registers.B.value = more;
+        comp.ram.setMemory(0, [12, 0, 1, 0]);
         comp.cpu.step();
 
-        expect(comp.cpu.registers.C.value).toBe(left ^ right);
+        expect(comp.cpu.flags.less).toBe(true);
+        expect(comp.cpu.flags.more).toBe(false);
+        expect(comp.cpu.flags.equal).toBe(false);
     });
+
+    it("register more", function () {
+        const comp = new Computer();
+        let less = 123;
+        let more = 456;
+
+        comp.cpu.registers.A.value = more;
+        comp.cpu.registers.B.value = less;
+        comp.ram.setMemory(0, [12, 0, 1, 0]);
+        comp.cpu.step();
+
+        expect(comp.cpu.flags.less).toBe(false);
+        expect(comp.cpu.flags.more).toBe(true);
+        expect(comp.cpu.flags.equal).toBe(false);
+    });
+
+    it("register equal", function () {
+        const comp = new Computer();
+        let value = 123;
+
+        comp.cpu.registers.A.value = value;
+        comp.cpu.registers.B.value = value;
+        comp.ram.setMemory(0, [12, 0, 1, 0]);
+        comp.cpu.step();
+
+        expect(comp.cpu.flags.less).toBe(false);
+        expect(comp.cpu.flags.more).toBe(false);
+        expect(comp.cpu.flags.equal).toBe(true);
+    });
+
 });
