d3935815c72cf54069c94bacf43459933887b38e
Ivan Chub <ivansergchub@gmail.com>
2016-03-01 08:05:11 -0800

    format

--- src/tests/AssemblerSpec.ts
+++ src/tests/AssemblerSpec.ts
@@ -5,7 +5,7 @@ import {ParamType} from "../ParamType";
 describe("assembler", function () {
     const assembler = new Assembler(new InstructionSet());
 
-    it("should work", function() {
+    it("should work", function () {
         const numbers = assembler.assembleString("LOAD 100 %B");
 
         expect(numbers).toEqual([2, 100, 1, 0]);
