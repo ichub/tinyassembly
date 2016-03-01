e085d70c2bf70e1e3a3882468dfc95c0d4cb92ea
Ivan Chub <ivansergchub@gmail.com>
2016-03-01 11:09:48 -0800

    made eventname explicitly a string

--- src/EventListenerDecorator.ts
+++ src/EventListenerDecorator.ts
@@ -1,11 +1,12 @@
 import {InterfaceHolder} from "./InterfaceHolder";
+import {EventName} from "./EventName";
 
-export function eventlistener(selector:string, event:string) {
+export function eventlistener(selector:string, event:string | EventName) {
     return function (target:InterfaceHolder, key:string, descriptor:PropertyDescriptor):PropertyDescriptor {
         target._binders = target._binders || [];
 
         target._binders.push((thisarg) => {
-            document.querySelector(selector).addEventListener(event, (e) => {
+            document.querySelector(selector).addEventListener(<string> event, (e) => {
                 target[key].apply(thisarg, [e]);
             });
         });
