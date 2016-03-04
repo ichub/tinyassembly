197489b4f4f0a833a4a776fe9116efa8a1d284d0
Ivan Chub <ivansergchub@gmail.com>
2016-03-03 19:07:58 -0800

    adding jump instruction test

--- src/tests/instructionSpecs/JumpSpec.ts
+++ src/tests/instructionSpecs/JumpSpec.ts
@@ -13,11 +13,35 @@ describe("the jump instruction", function () {
         });
 
         it("should not jump if not equal", function () {
+            comp.reset();
+
             comp.cpu.flags.equal = false;
             comp.ram.setMemory(0, [13, 100, 0, 0]);
             comp.cpu.step();
 
-            expect(comp.cpu.registers.IP.value).toBe(100);
+            expect(comp.cpu.registers.IP.value).toBe(comp.cpu.instructionSet.instructionLength);
         });
     });
+
+    describe("JUMPNEQ", function () {
+        it("should jump if not equal", function () {
+            comp.reset();
+
+            comp.cpu.flags.equal = false;
+            comp.ram.setMemory(0, [14, 100, 0, 0]);
+            comp.cpu.step();
+
+            expect(comp.cpu.registers.IP.value).toBe(100);
+        });
+
+        it("should jump if not equal", function () {
+            comp.reset();
+
+            comp.cpu.flags.equal = true;
+            comp.ram.setMemory(0, [14, 100, 0, 0]);
+            comp.cpu.step();
+
+            expect(comp.cpu.registers.IP.value).toBe(comp.cpu.instructionSet.instructionLength);
+        });
+    })
 });
