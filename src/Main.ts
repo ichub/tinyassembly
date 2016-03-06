a459e3b84fdbf7f8275464c80c403856cf2abf1e
Ivan Chub <ivansergchub@gmail.com>
2016-03-06 15:08:12 -0800

    testing out graphics

--- src/Main.ts
+++ src/Main.ts
@@ -5,6 +5,8 @@ import {RAM} from "./RAM";
 const computer = new Computer();
 const webInterface = new Interface(computer);
 
+computer.graphics.setMemory(0, [1, 1, 0, 0, 1, 1, 0, 0, 1, 1]);
+
 window.addEventListener("load", () => {
     webInterface.bind();
     computer.cpu.runSynchronouslyUntilHalted();
