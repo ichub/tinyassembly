d8082bbb653243eef2e9ce33ff6d015785bd8752
Ivan Chub <ivansergchub@gmail.com>
2016-03-13 16:37:38 -0700

    changed ram view a bit

--- src/components/RamViewComponent.tsx
+++ src/components/RamViewComponent.tsx
@@ -5,12 +5,14 @@ import {InstructionSet} from "../InstructionSet";
 import {RamRowComponent} from "./RamRowComponent";
 
 export class RamViewComponent extends React.Component<IComputerProps, any> {
+    private rowLength = 16;
+
     public render() {
         const ram = this.props.computer.ram;
         const values = [];
 
-        for (let i = 0; i < ram.size; i += 4) {
-            values.push(ram.getMemorySlice(i, 4));
+        for (let i = 0; i < ram.size; i += this.rowLength) {
+            values.push(ram.getMemorySlice(i, this.rowLength));
         }
 
         const rows = values.map((rowValues:number[], index:number) => {
