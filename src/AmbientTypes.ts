47e137d184e8dcabae12c49f57128b2edef62e3f
Ivan Chub <ivansergchub@gmail.com>
2016-03-16 08:32:04 -0700

    declare module events

--- src/AmbientTypes.ts
+++ src/AmbientTypes.ts
@@ -1,3 +1,10 @@
 interface ArrayConstructor {
     from<T, U>(arr:Array<T>, transform:(val:T) => U);
 }
+
+declare module "events" {
+    export abstract class EventEmitter {
+        public emit(event:string):void;
+        public on(event:string, handler:Function):void;
+    }
+}
