40ccd5f0153ac97e516457d953103cfb106138da
Ivan Chub <ivansergchub@gmail.com>
2016-03-26 17:16:37 -0700

    changed assembler size

--- src/components/AssemblyEditorComponent.tsx
+++ src/components/AssemblyEditorComponent.tsx
@@ -20,32 +20,32 @@ export class AssemblyEditorComponent extends React.Component<IAssemblyEditorProp
     private getProgramText() {
         // text printer
         /*
-        return [
-            "start:",
-            "LOAD #g_char_size %C",
-            "MUL 33 %C",
-            "ADD #m_static_low %C",
-            "LOAD 2 %A",
-            "LOAD 2 %B",
-            "loop:",
-            "BLIT %C %A %B",
-            "DRAW",
-            "ADD #g_char_width %A",
-            "INC %A",
-            "CMP 60 %A",
-            "JMPMEQ $else",
-            "LOAD 2 %A",
-            "ADD #g_char_height %B",
-            "INC %B",
-            "CMP 60 %B",
-            "JMPLEQ $end",
-            "else:",
-            "ADD #g_char_size %C",
-            "JMP $loop",
-            "end:",
-            "HALT",
-        ].join("\n");
-        */
+         return [
+         "start:",
+         "LOAD #g_char_size %C",
+         "MUL 33 %C",
+         "ADD #m_static_low %C",
+         "LOAD 2 %A",
+         "LOAD 2 %B",
+         "loop:",
+         "BLIT %C %A %B",
+         "DRAW",
+         "ADD #g_char_width %A",
+         "INC %A",
+         "CMP 60 %A",
+         "JMPMEQ $else",
+         "LOAD 2 %A",
+         "ADD #g_char_height %B",
+         "INC %B",
+         "CMP 60 %B",
+         "JMPLEQ $end",
+         "else:",
+         "ADD #g_char_size %C",
+         "JMP $loop",
+         "end:",
+         "HALT",
+         ].join("\n");
+         */
 
         // bouncing ball
 
@@ -75,8 +75,8 @@ export class AssemblyEditorComponent extends React.Component<IAssemblyEditorProp
                 <textarea
                     className="assembler-text"
                     ref="program"
-                    cols="30"
-                    rows="10"
+                    cols="50"
+                    rows="25"
                     defaultValue={this.getProgramText()}></textarea>
                 <br/>
                 <button onClick={this.handleAssembleButtonClick.bind(this)}>assemble</button>
