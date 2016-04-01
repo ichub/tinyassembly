dacb92cc4a98ab6bd1a2ee77c37e38951574fd3f
Ivan Chub <ivansergchub@gmail.com>
2016-04-01 16:46:14 -0700

    optimize drawing

--- src/DrawCache.ts
+++ src/DrawCache.ts
@@ -1,5 +1,6 @@
 import {ICachedDrawEvent} from "./CachedDrawEvent";
 import {DrawEventContext} from "./DrawEventContext";
+import {CachedClearEvent} from "./CachedClearEvent";
 
 export class DrawCache implements ICachedDrawEvent {
     private cachedDrawEvents:ICachedDrawEvent[];
@@ -12,7 +13,19 @@ export class DrawCache implements ICachedDrawEvent {
         this.cachedDrawEvents.push(event);
     }
 
+    private optimizeDrawEvents():void {
+        // remove all events before the last clear event
+        for (let i = this.cachedDrawEvents.length; i > 0; i--) {
+            if (this.cachedDrawEvents[i] instanceof CachedClearEvent) {
+                this.cachedDrawEvents = this.cachedDrawEvents.slice(i);
+                break;
+            }
+        }
+    }
+
     public apply(context:DrawEventContext):void {
+        this.optimizeDrawEvents();
+
         this.cachedDrawEvents.forEach((cachedEvent) => {
             cachedEvent.apply(context);
         });
