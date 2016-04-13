d0491b466e790e6f33591872c498bb7131bb1f99
Ivan Chub <ivansergchub@gmail.com>
2016-04-12 21:09:36 -0700

    added livereload

--- src/AmbientTypes.ts
+++ src/AmbientTypes.ts
@@ -1,3 +1,11 @@
 interface ArrayConstructor {
     from<T, U>(arr:Array<T>, transform:(val:T) => U);
 }
+
+declare module "livereload" {
+    export function createServer();
+
+    interface ILiveReloadServer {
+        watch(dir:string);
+    }
+}
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,13 +1,40 @@
 import * as express from "express";
 import * as path from "path";
+import * as hbs from "express-handlebars";
 
 const app = express();
+
+const isProduction = process.env.NODE_ENV === "production";
+
+app.engine(".hbs", hbs({
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "../../html/layouts/"),
+    defaultLayout: "main"
+}));
+
+app.set("view engine", ".hbs");
+app.set('views', path.join(__dirname, '../../html'));
+
+if (!isProduction) {
+    console.log("starting with development");
+
+    app.use(require("connect-livereload")({
+        port: 35729
+    }));
+
+    const livereload = require('livereload');
+    const livereloadServer = livereload.createServer();
+    livereloadServer.watch([
+        path.join(__dirname, "../../css"),
+        path.join(__dirname, "../../dist/bundle/bundle.js"),
+    ]);
+}
 
 app.use("/css", express.static(path.join(__dirname, "../../css")));
 app.use("/dist", express.static(path.join(__dirname, "../../dist")));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../html/index.html"));
+    res.render("index");
 });
 
 app.listen(process.env.PORT || 8080);
