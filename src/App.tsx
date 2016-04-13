0b4c046b9efb964131be6d782e65101e87347a8e
Ivan Chub <ivansergchub@gmail.com>
2016-04-12 19:08:37 -0700

    made cover and spinner disappear on page load

--- src/App.tsx
+++ src/App.tsx
@@ -4,4 +4,15 @@ import * as React from "react";
 /* tslint:enable:no-unused-variable */
 import {AppComponent} from "./components/AppComponent";
 
-ReactDom.render(<AppComponent/>, document.getElementById("app-container"));
+let hideCover = () => {
+    const cover = document.querySelector(".load-cover");
+    cover.classList.add("hidden");
+    cover.classList.remove("visible");
+};
+
+ReactDom.render(
+    <AppComponent/>,
+    document.getElementById("app-container"),
+    () => {
+        setTimeout(hideCover, 1000);
+    });
